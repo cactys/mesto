448a66805fdfc96ccfa92bc00e6827d04affbae4
cactys95 <cactys95@yandex.ru>
2022-05-13 01:47:48 +0300

    промежуточный из дома 2

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,21 +5,21 @@ const validationConfig = {
   inputErrorClass: '.form__input_inactive',
   submitButtonSelector: '.form__submit',
   inactiveButtonClass: '.form__submit_disabled',
-  errorText: '.form__input-error',
+  errorText: 'form__input-error',
   errorClass: 'form__input-error_active',
 };
 
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.quereSelectorAll(validationConfig.errorText);
-  inputElement.classList.add(validationConfig.inputErrorClass);
-  errorElement.classList.add(validationConfig.errorClass);
+  const errorElement = formElement.querySelector('.form__input-error');
+  inputElement.classList.add('form__input_inactive');
+  errorElement.classList.add('form__input-error_active');
   errorElement.textContent = errorMessage;
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.quereSelectorAll(validationConfig.errorText);
-  inputElement.classList.remove(validationConfig.inputErrorClass);
-  errorElement.classList.remove(validationConfig.errorClass);
+  const errorElement = formElement.querySelector('.form__input-error');
+  inputElement.classList.remove('form__input_inactive');
+  errorElement.classList.remove('form__input-error_active');
   errorElement.textContent = '';
 };
 
@@ -32,10 +32,8 @@ const checkInputValidity = (formElement, inputElement) => {
 };
 
 const setEventListeners = (formElement) => {
-  const inputList = Array.from(
-    formElement.quereSelectorAll(validationConfig.inputSelector)
-  );
-  const buttonElement = formElement.quereSelector(
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector(
     validationConfig.submitButtonSelector
   );
 
@@ -51,17 +49,13 @@ const setEventListeners = (formElement) => {
 };
 
 const enableValidation = () => {
-  const formList = Array.from(
-    document.querySelectorAll(validationConfig.formSelector)
-  );
+  const formList = Array.from(document.querySelectorAll('.form'));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
 
-    const fieldsetList = Array.from(
-      formElement.querySelectorAll(validationConfig.formSet)
-    );
+    const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
     fieldsetList.forEach((fieldset) => {
       setEventListeners(fieldset);
     });
@@ -76,9 +70,9 @@ const hasInvalidInput = (inputList) => {
 
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(validationConfig.inactiveButtonClass);
+    buttonElement.classList.add('form__submit_disabled');
   } else {
-    buttonElement.classList.remove(validationConfig.inactiveButtonClass);
+    buttonElement.classList.remove('form__submit_disabled');
   }
 };
 

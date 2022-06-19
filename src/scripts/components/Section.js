7c6a69410e62b6567ca2b82401510d7ec9ca4dd1
cactys95 <cactys95@yandex.ru>
2022-06-19 12:02:43 +0300

    fix: ревью

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -8,6 +8,21 @@ export default class Section {
   // принимает DOM-элемент и добавляет его в контейнер
   addItem(element) {
     this._container.prepend(element);
+    // Можно было бы сделать функцию renderer обычной функцией создания карточки
+    // (без вставки ее в DOM), тогда в методе addItem можно было бы сразу создавать
+    // карточку и тут же вставлять ее в DOM. Тогда в index.js не нужно было бы отдельно
+    // создавать функцию createCard, чтобы в 2х местах создавать карточки.
+    // ! addItem(item) {
+    // !    const card = this._renderer(item)
+    // !    this._container.prepend(card);
+    // ! }
+    // А можно переиспользовать метод _renderer, чтобы создавать и вставлять карточку
+    // ! renderCard(item) {
+    // !     this._renderer(item);
+    // ! }
+    // МОЖНО ЛУЧШЕ
+    // Gennadiy Barsegyan
+    // ревьюер
   }
 
   // метод, который отвечает за отрисовку всех элементов
